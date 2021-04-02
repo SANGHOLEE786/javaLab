6403ddfdc9697edf1b03a297c9514eb06e41463f
SangHo <SangHo@DESKTOP-6KRRDD4>
2021-04-02 13:02:12 +0900

    chap12

--- chap12Lab/src/sec09/exam01_execute_submit/ExecuteVsSubmitExample.java
+++ chap12Lab/src/sec09/exam01_execute_submit/ExecuteVsSubmitExample.java
@@ -6,8 +6,10 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 public class ExecuteVsSubmitExample {
 	public static void main(String[] args) throws Exception {
+		//1.스레드풀 생성
 		ExecutorService executorService = Executors.newFixedThreadPool(2);
-
+		
+		//2.작업생성 (익명구현객체로 task 생성)
 		for (int i = 0; i < 10; i++) {
 			Runnable runnable = new Runnable() {
 				@Override
@@ -22,12 +24,13 @@ public class ExecuteVsSubmitExample {
 				}
 			};
 
+			//3.작업큐에 작업처리 요청(작업큐에 적재시킨다.)
 			executorService.execute(runnable);
 			// executorService.submit(runnable);
 
 			Thread.sleep(10);
 		}
-
+		//4. 스레드풀 종료
 		executorService.shutdown();
 	}
 }
